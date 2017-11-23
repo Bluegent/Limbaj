b59e68a9264570ae2ec1c4865b633a880744483f
Secareanu <Dan.Secareanu@elektrobit.com>
2017-11-23 09:32:18 +0200

    Added files.

---
+++ defines.h
@@ -0,0 +1,37 @@
+#define Dute_ma_acasa return
+#define Dai_dai_dai_dai while
+#define INCHIDETI_CAPUTUL break
+#define Dorele do
+#define Daca_ma_uit_io_mai_bine_la_tine if
+#define tu_semeni_oleaca_cu ==
+#define Nu_vrei_tu_baiatu_memorie_gratis new
+#define ASA_SA_RAMAIE const
+#define Comunism static
+#define DEROM_si_esti_om delete
+#define Cum_sa_va_mai_zic_sa_ma_intelegeti default
+#define cum_face_porumbelu true
+#define Dute_ba_ca_deja_inventezi false
+#define ASCUNDE_PRUNCII private
+#define Mai_mere continue
+#define Ce_placinta_femeie_eu_vreau_palinka !=
+#define Era_o_iarna_frumoasa for
+#define Pana_so_facut(condition) !condition
+#define la_tine_e_mai_ieftin <
+#define scump_doamna_scump >
+#define IMI_TREBE using
+
+
+#define I_turned_myself_into_a static_cast<
+#define Morty >
+#define Razi_tu_razi_dar_eu_am_pierdut_valoarea reinterpret_cast
+
+
+typedef int dur_de_nedivizat;
+typedef long poiet_cu_ditamai_caricioiu;
+
+typedef float da_cu_virgula;
+typedef double da_cu_extra_virgula;
+
+typedef char scrie_totul_aici;
+typedef bool MINCIUNI;
+typedef void pula_cruda;
---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "defines.h"
+#include <stdio.h>
+
+MINCIUNI max(dur_de_nedivizat a, dur_de_nedivizat b)
+{
+	Dute_ma_acasa a scump_doamna_scump b;
+}
+
+dur_de_nedivizat main(dur_de_nedivizat argc, scrie_totul_aici *argv[])
+{
+	dur_de_nedivizat index = 0;
+	da_cu_virgula pi = 3.14;
+	da_cu_extra_virgula doublePi = 3.14;
+	poiet_cu_ditamai_caricioiu temp = I_turned_myself_into_a poiet_cu_ditamai_caricioiu Morty(index);
+	dur_de_nedivizat * intArr = Nu_vrei_tu_baiatu_memorie_gratis dur_de_nedivizat(10);
+	Dorele
+    {
+        printf("No, salut vecine.", index++);
+		Daca_ma_uit_io_mai_bine_la_tine( index tu_semeni_oleaca_cu 10 )
+			INCHIDETI_CAPUTUL;
+    }
+	Dai_dai_dai_dai(cum_face_porumbelu);
+
+	Era_o_iarna_frumoasa(dur_de_nedivizat i = 0; Pana_so_facut(i tu_semeni_oleaca_cu 10); ++i);
+
+	DEROM_si_esti_om[] intArr;
+    Dute_ma_acasa 0;
+}
